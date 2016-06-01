a4cd880ae2075c912d70313e0eee90e4a0c706e4
Stephen <stephen@umw.edu>
2016-06-01 12:06:07 -0400

    Next rev of user-set collection code.

--- collection/collectUserSet.R
+++ collection/collectUserSet.R
@@ -21,28 +21,51 @@ collect.user.set <- function(S, verbose=TRUE) {
         }
     }
 
+    # A vector of the screen names of everyone who follows anyone in the seed
+    # set.
     followers.of.S <- vector()
+
     for (s in S) {
-        this.seed.nodes.followers <- get.followers.of(s)
+        if (verbose) cat("Processing ",s,"...\n", sep="")
+        this.seed.nodes.followers <- get.followers.of.screenname(s)
         followers.of.S <- union(followers.of.S, this.seed.nodes.followers)
     }
 
+    if (verbose) cat("There are ", length(followers.of.S), 
+        " followers of the seed set.\n", sep="")
+
+    U <- vector()
     for (u in followers.of.S) {
+        if (verbose) cat("Checking ", u, "...\n", sep="")
+        this.users.followers <- get.followers.of.userid(u)
+        if (any(this.users.followers %in% followers.of.S)) {
+            if (verbose) cat("Including ", u, ".\n", sep="")
+            U <- union(U, u)
+        } else {
+            # if (verbose) cat("Excluding ", u, ".\n", sep="")
+        }
     }
 
-#    U.screen.names <- vector(length=length(U))
-#    for (chunk.num in 1:ceiling(length(U)/100)) {
-#        chunk.range <- (1+100*(chunk.num-1)):min(100*chunk.num,length(U))
-#        U.screen.names[chunk.range] <- get.screen.names(U[chunk.range])
-#    }
-    return(U.screen.names)
+    return(get.screennames(U))
 }
 
 
-get.followers.of <- function(screenname) {
-
+get.followers.of.screenname <- function(screenname, verbose=TRUE) {
+    if (verbose) cat("Getting ", screenname, "'s followers...\n", sep="")
     return(perform.cursor.call(paste0(
         "https://api.twitter.com/1.1/followers/ids.json?screen_name=",
+            screenname),"ids"))
+}
+
+get.followers.of.userid <- function(userid) {
+    return(perform.cursor.call(paste0(
+        "https://api.twitter.com/1.1/followers/ids.json?user_id=",
+            userid),"ids"))
+}
+
+get.followees.of <- function(screenname) {
+    return(perform.cursor.call(paste0(
+        "https://api.twitter.com/1.1/friends/ids.json?screen_name=",
             screenname),"ids"))
 }
 
@@ -50,25 +73,37 @@ perform.cursor.call <- function(url, field.to.extract) {
     results.so.far <- vector()
     the.call <- make.manual.twitter.api.call(paste0(
         url, "&cursor=-1"))
+    if ("error" %in% names(the.call)) {
+        cat(the.call[["error"]],"\n")
+        return(NULL)
+    }
     results.so.far <- union(results.so.far, the.call[[field.to.extract]])
     cursor <- the.call$next_cursor
-    while (cursor != 0) {
+    while (!is.null(cursor) && cursor != 0) {
         the.call <- make.manual.twitter.api.call(paste0( url, 
             "&cursor=", cursor))
         results.so.far <- union(results.so.far, the.call[[field.to.extract]])
+        cursor <- the.call$next_cursor
     }
     return(results.so.far)
 }
 
 
-get.screen.names <- function(userids) {
-    lookup.call <- make.manual.twitter.api.call(
-        paste0("https://api.twitter.com/1.1/users/lookup.json?user_id=",
-            paste(userids,collapse=",")))
-    return(lookup.call$screen_name)
+get.screennames <- function(userids, verbose=TRUE) {
+    if (verbose) cat("Getting ",length(userids), " screennames...\n", sep="")
+    screennames <- vector(length=length(userids))
+    for (chunk.num in 1:ceiling(length(userids)/100)) {
+        if (verbose) cat("chunk number ",chunk.num, "...\n", sep="")
+        chunk.range <- (1+100*(chunk.num-1)):min(100*chunk.num,length(userids))
+        lookup.call <- make.manual.twitter.api.call(
+            paste0("https://api.twitter.com/1.1/users/lookup.json?user_id=",
+                paste(userids[chunk.range],collapse=",")))
+        screennames[chunk.range] <- lookup.call$screen_name
+    }
+    return(screennames)
 }
 
 
 main <- function() {
-    U <<- collect.user.set(c("hzontine","rockladyeagles"))
+    U <<- collect.user.set(c("rockladyeagles","hzontine"))
 }
