6fb74724b92cb192fd4123b50b363fd6164c1184
Stephen <stephen@umw.edu>
2016-06-29 12:38:55 -0400

    Verify Zontine variant converges faster than Holley.

--- synthetic/reproduceLitModels.R
+++ synthetic/reproduceLitModels.R
@@ -1,21 +1,92 @@
 # Reproduce results of published papers
 
+library(ggplot2)
+
 source("opinionDynamics.R")
 
-binary.voter <- function() {
-    # Holley and Liggett 1975        Clifford and Sudbury 1973
-    # Vertex always changes her opinion to reflect that of the victim's opinion
-    # Only one encounter per iteration
-    # Results: Opinions will always converge to a consensus
-    set.seed(111234)
-    init <- get.plain.old.graph(opinion=rbinom(50,1,0.5), probability.connected=0.2)
+# Holley and Liggett 1975        Clifford and Sudbury 1973
+# Vertex always changes her opinion to reflect that of the victim's opinion
+# Only one encounter per iteration
+# Results: Opinions will always converge to a consensus
+#
+# choose.randomly.each.encounter -- if TRUE, this is Holley's original: each
+# encounter will have a completely random "encountering vertex" chosen. If
+# FALSE, this is the Zontine variant: all the vertices will be chosen as the
+# "encountering vertex" (in random order) before they are all chosen again
+# (i.e., no repeats are allowed.)
+#
+binary.voter <- function(choose.randomly.each.encounter=FALSE, plot=TRUE) {
+    #set.seed(111234)
+    init <- get.plain.old.graph(opinion=rbinom(50,1,0.5), 
+        probability.connected=0.2)
     graphs <<- sim.opinion.dynamics(init, num.encounters=50*vcount(init),
         encounter.func=get.graph.neighbors.encounter.func(1),
-        victim.update.function=get.automatically.update.victim.function(A.is.victim=TRUE), 
-        choose.randomly.each.encounter=FALSE)
-    plot.animation(graphs, "opinion", delay.between.frames=.15)
+        victim.update.function=get.automatically.update.victim.function(
+                                                            A.is.victim=TRUE), 
+        choose.randomly.each.encounter=choose.randomly.each.encounter,
+        verbose=FALSE)
+    if (plot) {
+        plot.animation(graphs, "opinion", delay.between.frames=.15)
+    }
+    return(graphs)
 }
 
+
+# Verify that choosing each (shuffled) vertex in sequence before choosing each
+# one again reaches consensus faster than the original Holley version in which
+# every encounter chooses a new influencing vertex afresh.
+#
+# results -- if NULL, re-run the parameter sweep from scratch. If not, results
+# should be the return value from a previous call to this function.
+#
+# num.trials -- how many simulations of each of the two variants to run.
+#
+# Returns: a data frame with two columns: choose.randomly (boolean; FALSE
+# indicates the Zontine variant) and iter.to.consensus (the number of
+# iterations it took for the graph to reach uniformity; Inf if it never did in
+# the required number of iterations.)
+
+verify.zontine.variant.converges.faster <- function(results=NULL,
+    num.trials=20) {
+
+    library(doParallel)
+    registerDoParallel(8)
+
+    if (is.null(results)) {
+        pair.of.result.sets <- 
+          lapply(c(TRUE,FALSE), function(choose.randomly.each.encounter) {
+            results <- foreach(trial=1:num.trials, .combine=rbind) %dopar% {
+                cat("\n----------------------------\n")
+                cat("Trial #",trial,"\n",sep="")
+                graphs <- binary.voter(choose.randomly.each.encounter,
+                    plot=FALSE)
+                num.iter.before.consensus <- Inf
+                for (iter in 1:length(graphs)) {
+                    if (length(unique(V(graphs[[iter]])$opinion)) == 1) {
+                        num.iter.before.consensus <- iter
+                        break
+                    }
+                }
+                return(data.frame(choose.randomly.each.encounter, 
+                    num.iter.before.consensus))
+            }
+        })
+        results <- as.data.frame(
+            rbind(pair.of.result.sets[[1]],pair.of.result.sets[[2]]))
+        rownames(results) <- 1:nrow(results)
+        colnames(results) <- c("choose.randomly","iter.to.consensus")
+    }
+
+    print(ggplot(results, aes(x=choose.randomly, y=iter.to.consensus,
+            fill=choose.randomly)) +
+        geom_boxplot(notch=TRUE) +
+        ggtitle(paste0("Holley vs. Zontine variants (n=",2*num.trials,")")) +
+        ylab("# of iterations to convergence") +
+        scale_fill_discrete(name="Variant", breaks=c(TRUE,FALSE),
+            labels=c("Holley","Zontine")))
+    
+    return(results)
+}
 
 yildiz.binary <- function(){
     # Yildoz, Acemoglu, et al. 2013
@@ -35,4 +106,3 @@ yildiz.binary <- function(){
     plot.animation(binary.voter.stubborn.graph, "opinion", delay.between.frames=.25)
 }
 
-dave.model <- 
