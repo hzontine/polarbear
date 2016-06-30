c437f61661b24ab7e3150dc4f56db8edb9a3d957
Stephen <stephen@umw.edu>
2016-06-30 15:25:23 -0400

    Change "first" to "first.g".

--- synthetic/pelRyan.R
+++ synthetic/pelRyan.R
@@ -5,9 +5,10 @@ pelryan <- function() {
     #download.file("http://cs.umw.edu/~stephen/pelosiRyan.RData", "pelosiRyan.RData")
     #load("pelosiRyan.RData")
 
-    first <<- U
-    V(first)$opinion <<- sample(c(0,1),vcount(first),replace=TRUE)
-    pel.graphs <<- sim.opinion.dynamics(first, num.encounters=219852,
+    first.g <<- pelosi.ryan
+    V(first.g)$opinion <<- sample(c(0,1),vcount(first.g),replace=TRUE)
+    pel.graphs <<- sim.opinion.dynamics(first.g,
+        num.encounters=vcount(first.g)*4,
         encounter.func=get.mean.field.encounter.func(2),
         victim.update.function=get.automatically.update.victim.function(),
         edge.update=FALSE,
