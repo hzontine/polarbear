1cdead1810c854464be42c9423a502a94a73dce9
Hannah Zontine <hzontine@Hannahs-MacBook-Pro.local>
2016-06-30 11:31:23 -0400

    PelRyan runs but takes a long time

--- synthetic/pelRyan.R
+++ synthetic/pelRyan.R
@@ -2,15 +2,16 @@ source("opinionDynamics.R")
 
 pelryan <- function() {
 
-    #download.file("http://cs.umw.edu/~stephen/pelosiRyan.RData", "pelRyan.rData")
-    #load("pelRyan.rData")
+    #download.file("http://cs.umw.edu/~stephen/pelosiRyan.RData", "pelosiRyan.RData")
+    #load("pelosiRyan.RData")
 
     first <<- U
-    V(first)$opinion <- sample(c(0,1),vcount(first),replace=TRUE)
-    pel.graphs <<- sim.opinion.dynamics(first, num.encounters=403062,
-        encounter.func=get.graph.neighbors.encounter.func(vcount(first)),
-        victim.update.function=get.automatically.update.function(),
-        edge.update=FALSE)
+    V(first)$opinion <<- sample(c(0,1),vcount(first),replace=TRUE)
+    pel.graphs <<- sim.opinion.dynamics(first, num.encounters=219852,
+        encounter.func=get.mean.field.encounter.func(2),
+        victim.update.function=get.automatically.update.victim.function(),
+        edge.update=FALSE,
+        choose.randomly.each.encounter=TRUE)
     plot.animation(pel.graphs,"opinion",delay.between.frames=.15)
 
 }
