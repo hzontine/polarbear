294c2eb093c676b76fe0f3a3ed3cfce6e54a97c2
Hannah Zontine <hzontine@Hannahs-MacBook-Pro.local>
2016-07-05 17:47:10 -0400

    Added summerplan.R

--- synthetic/summerPlan.R
+++ synthetic/summerPlan.R
@@ -1,18 +1,22 @@
 source("opinionDynamics.R")
 
+# Binary Opinions
+# Graph does not change
+
 pelryan <- function() {
 
     #download.file("http://cs.umw.edu/~stephen/pelosiRyan.RData", "pelosiRyan.RData")
-    #load("pelosiRyan.RData")
+    #U <- load("pelosiRyan.RData")
 
-    first.g <<- pelosi.ryan
+    first.g <<- U
     V(first.g)$opinion <<- sample(c(0,1),vcount(first.g),replace=TRUE)
     pel.graphs <<- sim.opinion.dynamics(first.g,
-        num.encounters=vcount(first.g)*4,
-        encounter.func=get.mean.field.encounter.func(2),
+        num.encounters=vcount(first.g)*2,
+        encounter.func=get.graph.neighbors.encounter.func(1),
         victim.update.function=get.automatically.update.victim.function(),
         edge.update=FALSE,
         choose.randomly.each.encounter=TRUE)
     plot.animation(pel.graphs,"opinion",delay.between.frames=.15)
-
+    plot.polarization(pel.graphs, "opinion")
+    plot.binary.opinions(pel.graphs)
 }
