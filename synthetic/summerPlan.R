95863ddc48e9645c30a0aed9de31a0c0d0da534b
Hannah Zontine <hzontine@Hannahs-MacBook-Pro.local>
2016-07-08 13:20:53 -0400

    Fixed graphA

--- synthetic/summerPlan.R
+++ synthetic/summerPlan.R
@@ -12,14 +12,14 @@ pelryan <- function() {
     first.g <<- erdos.renyi.game(100,0.05)
     V(first.g)$opinion <<- sample(c(0,1),vcount(first.g),replace=TRUE)
     pel.graphs <<- sim.opinion.dynamics(first.g,
-        num.encounters=10,
+        num.encounters=40,
         encounter.func=get.graph.neighbors.encounter.func(1),
-        victim.update.function=get.automatically.update.victim.function(A.is.victim=TRUE),
+        victim.update.function=get.automatically.update.victim.function(A.is.victim=FALSE),
         edge.update=FALSE,
         majority=FALSE,
         choose.randomly.each.encounter=FALSE)
-    plot.animation(pel.graphs,"opinion",delay.between.frames=.15)#, 
-        #interactive=FALSE, animation.filename="graphA.gif")
+    plot.animation(pel.graphs,"opinion",delay.between.frames=.15, 
+        interactive=FALSE, animation.filename="graphA.gif")
     plot.polarization(pel.graphs, "opinion")
     plot.binary.opinions(pel.graphs)
 }
