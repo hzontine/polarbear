34af1d80a1603ca84c6b913a4be3cecf0f27795f
Stephen <stephen@umw.edu>
2016-07-11 13:54:47 -0400

    Refactor term associations.

--- classifier/analyze.R
+++ classifier/analyze.R
@@ -21,38 +21,19 @@ load("trainingData.RData")
 # dtm -- if this argument is provided, the doc-term matrix will not be
 # renegerated.
 
-build.classifier <- function(classification=1,dtm=NULL) {
+build.classifier <- function(classification=1,dtm=NULL,data=training.data) {
 
     class.column.name <- paste0("daclass",classification)
-    classes <- training.data[[class.column.name]]
+    classes <- data[[class.column.name]]
 
     # Count "Not sure" as NA.
     classes <- ifelse(classes == "Not sure", NA, classes)
 
     if (is.null(dtm)) {
-
-        cat("Preprocessing tweets...\n")
-
-        # Dave processing.
-        docs <- training.data$content
-
-        #docs <- iconv(docs, sub="")
-        docs <- gsub("[^\x20-\x7E]", "", docs)
-        docs <- gsub("[^a-z]"," ",tolower(docs))
-        docs <- gsub("\\s+"," ",docs)
-        docs <- gsub("(^\\s)|(\\s$)","",docs)
-
-        training.size <- sum(!is.na(classes))
-
-        cat("Generating doc-term matrix...\n")
-        dtm0 <- DocumentTermMatrix(Corpus(VectorSource(docs)),
-           control=list(weighting=weightTfIdf,
-                        removePunctuation=FALSE,
-                        removeDigits=FALSE,
-                        stemming=FALSE,
-                        wordLengths=c(3,Inf)))
-        dtm <- removeSparseTerms(dtm0,.99)
+        dtm <- build.dtm(data)
     }
+
+    training.size <- sum(!is.na(classes))
 
     training.only <- cbind(as.data.frame(as.matrix(dtm)[!is.na(classes),]),
             daclass=as.factor(classes)[!is.na(classes)])
@@ -69,14 +50,43 @@ build.classifier <- function(classification=1,dtm=NULL) {
     return(list(dtm=dtm,model=rf.model))
 }
 
+build.dtm <- function(data=training.data) {
 
-assocs <- function() {
-    class.stuff <- build.classifier(1)
+    cat("Preprocessing tweets...\n")
+
+    # Dave processing.
+    docs <- data$content
+
+    #docs <- iconv(docs, sub="")
+    docs <- gsub("[^\x20-\x7E]", "", docs)
+    cat("1 of 4...\n")
+    docs <- gsub("[^a-z]"," ",tolower(docs))
+    cat("2 of 4...\n")
+    docs <- gsub("\\s+"," ",docs)
+    cat("3 of 4...\n")
+    docs <- gsub("(^\\s)|(\\s$)","",docs)
+    cat("4 of 4.\n")
+
+    cat("Generating doc-term matrix...\n")
+    dtm0 <- DocumentTermMatrix(Corpus(VectorSource(docs)),
+       control=list(weighting=weightTfIdf,
+                    removePunctuation=FALSE,
+                    removeDigits=FALSE,
+                    stemming=FALSE,
+                    wordLengths=c(3,Inf)))
+    dtm <- removeSparseTerms(dtm0,.99)
+    return(dtm)
+}
+
+assocs <- function(dtm=NULL) {
+    if (is.null(dtm)) {
+        dtm <- build.classifier(1)$dtm
+    }
     term <- readline("Enter a term (or 'done'): ")
     while (term != "done") {
-        print(sort(
-            findAssocs(classstuff$dtm,term,rep(.1,1))[[1]],
-            decreasing=TRUE)[1:20])
+        associations <- findAssocs(dtm,term,rep(.05,1))[[1]]
+        print(sort(associations,decreasing=TRUE)[
+            1:(min(length(associations),20))])
         term <- readline("Enter a term (or 'done'): ")
     }
 }
