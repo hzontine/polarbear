940bc5ea7ecb2dc8c30ee231ddd15757284d8293
Stephen <stephen@umw.edu>
2016-09-29 13:48:54 -0400

    Add networkParadigm.R.

---
+++ friedkin/networkParadigm.R
@@ -0,0 +1,42 @@
+
+# Network paradigm, p.194
+# Super flexible: all coefficients and scaling factors differ per time period.
+
+# number of agents
+n <- 10
+
+# number of exogenous variables
+k <- 5
+
+# how long to run simulation
+num.iter <- 100
+
+
+# time-varying matrix of coefficient of effects of agents on each other
+W <- array(runif(n*n*num.iter,min=-1,max=1),dim=c(n,n,num.iter))
+
+# time-varying matrix of exogenous scores
+X <- array(runif(n*k*num.iter,min=-1,max=1),dim=c(n,k,num.iter))
+
+# time-varying matrix of exogenous coefficients
+B <- matrix(runif(k*1*num.iter,min=-1,max=1),nrow=k)
+
+# scaling factors (variable over time)
+alpha <- runif(num.iter)
+beta <- runif(num.iter)
+
+# initial condition
+Y.init <- X[,,1] %*% B[,1]
+
+Y <- matrix(0,nrow=n,ncol=num.iter)
+Y[,1] <- Y.init
+
+
+for (i in 2:num.iter) {
+    Y[,i] <- W[,,i] %*% Y[,i-1] + X[,,i] %*% B[,i]
+}
+
+plot(1:num.iter,Y[1,],type="n",ylim=c(min(Y),max(Y)))
+for (i in 1:n) {
+    lines(1:num.iter,Y[i,],type="l")
+}
