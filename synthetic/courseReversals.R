008a589540cd3eefaffa736fa329292c96075b6a
Hannah Zontine <hzontine@mail.umw.edu>
2016-11-09 19:15:56 -0500

    Have detect course rehearsals return Inf for simulations where opinion never completely unified

--- synthetic/courseReversals.R
+++ synthetic/courseReversals.R
@@ -50,136 +50,146 @@ detect.course.reversal <- function(graphs){
 
 
 # HIDDEN ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        # if the number of agents whose HIDDEN opinion == 0 is 
-        # greater than half of the total number of agents
-        if(length(which(hidden == 0)) > (num / 2)){ 
-            # 0 is the max and 1 is the min
-            # what is the maximum percentage 1 ever got to?
-            # if any instances of 1 greater than 50%
-            max <- 50.0
-            for(g in 1:length(graphs)){
-                h <- sapply(1:num, function(x) get.vertex.attribute(graphs[[g]], 
-                    "hidden", V(graphs[[g]])[x]))
-                percentage <- length(which(h == 1)) / num
-                if(percentage > max){
-                    max <- percentage
-                }
-            }
-            hidden.result <- max
-        } else{ # if hidden is split in half evenly
-            # should literally never happen bec we terminate after unanimity
-            if(length(which(hidden == 0)) == (num / 2)){ 
-                hidden.result <- 0
-            } else { # if the number of agents whose hidden opinion is 0 is less than
-                     # half of the total number of agents (AKA more 1s than 0s)
-                     # 1 is the max and 0 is the min
-                
-                #what is the highest percentage that 0 ever got to?
-                max <- 50.0
-                for( g in 1:length(graphs)){
-                    h <- sapply(1:num, function(x) get.vertex.attribute(graphs[[g]], 
-                    "hidden", V(graphs[[g]])[x]))
-                    percentage <- length(which(h == 0)) / num
-                    if(percentage > max){
-                        max <- percentage
-                    }
-                }
-                hidden.result <- max
-            }
-        }
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# EXPRESSED ~~~~~~~~~~~~~~~~~~~~~~~~~
+	# If it never converges
+	if(length(unique(hidden)) == 0){
+		hidden.result <- Inf
+	}else{
+	        # if the number of agents whose HIDDEN opinion == 0 is 
+        	# greater than half of the total number of agents
+        	if(length(which(hidden == 0)) > (num / 2)){ 
+		    # 0 is the max and 1 is the min
+		    # what is the maximum percentage 1 ever got to?
+		    # if any instances of 1 greater than 50%
+		    max <- 50.0
+		    for(g in 1:length(graphs)){
+			h <- sapply(1:num, function(x) get.vertex.attribute(graphs[[g]], 
+			    "hidden", V(graphs[[g]])[x]))
+			percentage <- length(which(h == 1)) / num
+			if(percentage > max){
+			    max <- percentage
+			}
+		    }
+		    hidden.result <- max
+		} else{ # if hidden is split in half evenly
+		    # should literally never happen bec we terminate after unanimity
+		    if(length(which(hidden == 0)) == (num / 2)){ 
+			hidden.result <- 0
+		    } else { # if the number of agents whose hidden opinion is 0 is less than
+			     # half of the total number of agents (AKA more 1s than 0s)
+			     # 1 is the max and 0 is the min
+			
+			#what is the highest percentage that 0 ever got to?
+			max <- 50.0
+			for( g in 1:length(graphs)){
+			    h <- sapply(1:num, function(x) get.vertex.attribute(graphs[[g]], 
+			    "hidden", V(graphs[[g]])[x]))
+			    percentage <- length(which(h == 0)) / num
+			    if(percentage > max){
+				max <- percentage
+			    }
+			}
+			hidden.result <- max
+		    }
+		}
+	}
 
-        # if the number of agents whose expressed == 0 is 
-        # greater than half of the total number of agents
-        if(length(which(expressed == 0)) > (num / 2)){ 
-            # 0 is the max and 1 is the min
-            max <- 50.0
-            # what is the highest percentage that 1 ever got to?
-            for(g in 1:length(graphs)){
-                ex <- sapply(1:num, function(x) get.vertex.attribute(graphs[[g]], 
-                "expressed", V(graphs[[g]])[x]))
-                percentage <- length(which(ex == 1)) / num
-                if(percentage > max){
-                    max <- percentage
-                }
-            }
-            expressed.result <- max
-        } else{ # if opinion is split in half evenly
-            if(length(which(expressed == 0)) == (num / 2)){ 
-                # should literally never happen
-                expressed.result <- 0
-            } else { # if the number of agents whose opinion is 0 is less than
-                     # half of the total number of agents (AKA more 1s than 0s)
-                     # 1 is the max and 0 is the min
-                # was 0 ever the max?
-                max <- 50.0
-                # what is the highest percentage that 0 ever got to?
-                for (g in 1:length(graphs)){
-                    ex <- sapply(1:num, function(x) get.vertex.attribute(graphs[[g]], 
-                    "expressed", V(graphs[[g]])[x]))
-                    percentage <- length(which(ex == 0)) /num
-                    if(percentage > max){
-                        max <- percentage
-                    }
-                }
-		        expressed.result <- max
-            }
-        }
- 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# EXPRESSED ~~~~~~~~~~~~~~~~~~~~~~~~~
+	if(length(unique(expressed)) == 0){
+		expressed.result <- Inf
+	} else{
+		# if the number of agents whose expressed == 0 is 
+		# greater than half of the total number of agents
+		if(length(which(expressed == 0)) > (num / 2)){ 
+		    # 0 is the max and 1 is the min
+		    max <- 50.0
+		    # what is the highest percentage that 1 ever got to?
+		    for(g in 1:length(graphs)){
+			ex <- sapply(1:num, function(x) get.vertex.attribute(graphs[[g]], 
+			"expressed", V(graphs[[g]])[x]))
+			percentage <- length(which(ex == 1)) / num
+			if(percentage > max){
+			    max <- percentage
+			}
+		    }
+		    expressed.result <- max
+		} else{ # if opinion is split in half evenly
+		    if(length(which(expressed == 0)) == (num / 2)){ 
+			# should literally never happen
+			expressed.result <- 0
+		    } else { # if the number of agents whose opinion is 0 is less than
+			     # half of the total number of agents (AKA more 1s than 0s)
+			     # 1 is the max and 0 is the min
+			# was 0 ever the max?
+			max <- 50.0
+			# what is the highest percentage that 0 ever got to?
+			for (g in 1:length(graphs)){
+			    ex <- sapply(1:num, function(x) get.vertex.attribute(graphs[[g]], 
+			    "expressed", V(graphs[[g]])[x]))
+			    percentage <- length(which(ex == 0)) /num
+			    if(percentage > max){
+				max <- percentage
+			    }
+			}
+			expressed.result <- max
+		    }
+		}
+	}
+	 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         return(c(hidden.result, expressed.result))
 
     }else{
-	op1 <- sapply(1:num, function(x) get.vertex.attribute(graphs[[1]], "opinion",
-		V(graphs[[1]])[x]))
-	cat("Starting table in CR: \n",table(op1), "\n")
-
 
         # find vector of opinion values that represent the last graph
         opinion <- sapply(1:num, function(x) get.vertex.attribute(graphs[[length(graphs)]], 
             "opinion", V(graphs[[length(graphs)]])[x]))
-        
-        # if the number of agents whose opinion is 0 is greater than
-        # half of the total number of agents
-        if(length(which(opinion == 0)) > (num / 2)){ 
-            # 0 is the max and 1 is the min
-            # so we need to find the maximum percentage that 1 ever had?
-            max <- 0
-            # for each graph in graphs, what is the highest 1 ever got to?
-            for(g in 1:length(graphs)){
-                op <- sapply(1:num, function(x) get.vertex.attribute(graphs[[g]], 
-                "opinion", V(graphs[[g]])[x]))
+	if(length(unique(opinion)) == 0){
+		return(Inf)
+	}else{        
+		# if the number of agents whose opinion is 0 is greater than
+		# half of the total number of agents
+		if(length(which(opinion == 0)) > (num / 2)){ 
+		    # 0 is the max and 1 is the min
+		    # so we need to find the maximum percentage that 1 ever had?
+		    max <- 0
+		    # for each graph in graphs, what is the highest 1 ever got to?
+		    for(g in 1:length(graphs)){
+			op <- sapply(1:num, function(x) get.vertex.attribute(graphs[[g]], 
+			"opinion", V(graphs[[g]])[x]))
 
-                percentage <- length(which(op == 1)) / num 
-                if(percentage > max){
-                    	max <- percentage
-                }
-            }
-            return(max)
-        } else { # if opinion is split in half evenly
-            if(length(which(opinion == 0)) == (length(opinion) / 2)){ 
-                # should never happen
-                return (0) 
-            } else { # if the number of agents whose opinion is 0 is less than
-                     # half of the total number of agents (AKA more 1s than 0s)
-                     # 1 is the max and 0 is the min
+			percentage <- length(which(op == 1)) / num 
+			if(percentage > max){
+				max <- percentage
+			}
+		    }
 
-                    max <- 0
-                    # what is the maximum percentage that 0 ever reached?
-                    for(g in 1:length(graphs)){
-                        op <- sapply(1:num, function(x) get.vertex.attribute(graphs[[g]], 
-                            "opinion", V(graphs[[g]])[x]))
-                        percentage <- length(which(op == 0)) / num
-                        if(percentage > max){
-                            	max <- percentage
-                        }
-                    }
-                    return(max)
-            }
-        }
+		    return(max)
+
+		} else { # if opinion is split in half evenly
+		    if(length(which(opinion == 0)) == (length(opinion) / 2)){ 
+			# should never happen
+			return (0) 
+		    } else { # if the number of agents whose opinion is 0 is less than
+			     # half of the total number of agents (AKA more 1s than 0s)
+			     # 1 is the max and 0 is the min
+
+			    max <- 0
+			    # what is the maximum percentage that 0 ever reached?
+			    for(g in 1:length(graphs)){
+				op <- sapply(1:num, function(x) get.vertex.attribute(graphs[[g]], 
+				    "opinion", V(graphs[[g]])[x]))
+				percentage <- length(which(op == 0)) / num
+				if(percentage > max){
+					max <- percentage
+				}
+			    }
+			    return(max)
+		    }
+		}
+	}
     }
-
 }
 
 
