c77ff7ea7e5d368914badd6047b1edd891e476ca
Hannah Zontine <hzontine@cs.umw.edu>
2017-04-05 13:04:14 -0400

    Update prob plots with fixed encounters

--- synthetic/opinionDynamics.R
+++ synthetic/opinionDynamics.R
@@ -847,7 +847,7 @@ get.expressed.latent.graph <- function(num.agents=100, prob.connected=0.2, dir=F
 
                 
 #default number of seeds is 50 
-param.sweep <- function(seeds=seq(10,500,10), update=seq(.1,1,0.05)) {
+param.sweep <- function(seeds=seq(10,500,10), pp=0.5, update=0.5, intern=0.5, con=0.25) {
 
   library(doParallel)
   registerDoParallel(60)
@@ -855,19 +855,19 @@ param.sweep <- function(seeds=seq(10,500,10), update=seq(.1,1,0.05)) {
   bias.list <- list()
   firstRun <- TRUE
   # for each value of peer pressure probability, compute poll bias
-  for(i in 1:length(update)){
-    prob <- update[i]
+  for(i in 1:length(intern)){
+    prob <- intern[i]
     
     bias.data <- foreach(num=1:length(seeds), .combine = 'cbind') %dopar% {
         # run the same seeds for each probability
         set.seed(seeds[num])
-        initial.graph <- get.expressed.latent.graph(num.agents = 64, prob.connected = .25, dir = FALSE)
+        initial.graph <- get.expressed.latent.graph(num.agents = 64, prob.connected = con, dir = FALSE)
         graphs <- sim.opinion.dynamics(initial.graph, num.encounters=50*vcount(initial.graph),
                                      encounter.func=list(get.mean.field.encounter.func(1),
                                        get.graph.neighbors.encounter.func(1)),
                                      victim.update.function=list(get.automatically.update.victim.function(A.is.victim=TRUE,
-                                        prob.update=prob, opinion.type="hidden"), get.peer.pressure.update.function(A.is.victim=TRUE,
-                                        prob.knuckle.under.pressure=0.5, prob.internalize.expressed.opinion=0.5, trumpEffect=TRUE)),
+                                        prob.update=update, opinion.type="hidden"), get.peer.pressure.update.function(A.is.victim=TRUE,
+                                        prob.knuckle.under.pressure=pp, prob.internalize.expressed.opinion=prob, trumpEffect=TRUE)),
                                      generate.graph.per.encounter=TRUE, verbose = TRUE,
                                      termination.function=get.never.terminate.function(),
                                     terminate.after.max.num.encounters=TRUE,
@@ -899,8 +899,8 @@ param.sweep <- function(seeds=seq(10,500,10), update=seq(.1,1,0.05)) {
   return(bias.list)
 }
 
-#data <- param.sweep()
-#save(data, file="updateProbData.RData")
+data <- param.sweep(intern=seq(0,1,0.05))
+save(data, file="internProbData.RData")
 
 
 main <- function() {
