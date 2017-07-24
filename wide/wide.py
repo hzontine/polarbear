b26b05a8cdfe35a6d60d989500fd78c7f9e019ef
Stephen <stephen@umw.edu>
2017-07-24 10:54:10 -0400

    Add generate_friends_graph() and other scaffolding.

--- wide/wide.py
+++ wide/wide.py
@@ -6,11 +6,30 @@ import logging
 
 logging.getLogger().setLevel('INFO')
 
+# Configuration parameters.
 N = 50
-NUM_ITER = 1000
+NUM_ITER = 100
+NUM_IDEOLOGIES = 3
 
-graph = igraph.Graph.Erdos_Renyi(N, .1)
-graph.vs['color'] = [random.choice(['red','green','blue']) for _ in range(N)]
+
+def generate_friends_graph(associates_graph, openness=.5):
+    '''
+    Given a graph of associations (i.e., whose edges represent the random
+    starting material that people get as far as who they encounter), produce a
+    graph of friendships, where each of a node's friendships are either chosen
+    from their associates, or from the graph at large, depending on the
+    openness parameter passed.
+    '''
+    return associates_graph
+
+
+POSSIBLE_COLORS = ['red','blue','green','brown','purple']
+colors = POSSIBLE_COLORS[:NUM_IDEOLOGIES]
+
+associates_graph = igraph.Graph.Erdos_Renyi(N, .1)
+associates_graph.vs['color'] = [ random.choice(colors) for _ in range(N) ]
+graph = generate_friends_graph(associates_graph)
+
 
 # Static layout: compute once and use, since graph structure doesn't change.
 layout = graph.layout_kamada_kawai(seed=None)
