4ff35e13c09ccadee52727dace8e29bab1c06c13
Stephen <stephen@umw.edu>
2017-07-26 11:16:11 -0400

    Use keyword-based command-line args.

--- wide/main_wide.py
+++ wide/main_wide.py
@@ -10,79 +10,67 @@ from wide import *
 from wide_sim import *
 
 
+def print_usage():
+    print('Usage: main_wide.py [env_openness=#] [tolerance=#] [sweep=#] ' +
+        '[seed=#] [logLevel=level|NONE].')
+
 if len(sys.argv) > 5:
-    print('Usage: main_wide.py [env_openness] [tolerance] [sweep=#] [seed] ' +
-        '[logLevel|NONE].')
+    print_usage()
     sys.exit(1)
 
-if len(sys.argv) > 5:
-    if sys.argv[5] == 'NONE':
-        logging.getLogger().setLevel(logging.CRITICAL + 1)
-    else:
-        logging.getLogger().setLevel(sys.argv[5])
-else:
-    logging.getLogger().setLevel('INFO')
+this_module = sys.modules[__name__]
 
-if len(sys.argv) > 4:
-    try:
-        seed = int(sys.argv[4])
-        if seed == 0:
-            seed = random.randrange(10000)
-    except:
+for arg in sys.argv[1:]:
+    if not '=' in arg:
+        print("Malformed argument '{}'.".format(arg))
         print_usage()
-        print("('seed' must be numeric.)")
-        sys.exit(4)
+        sys.exit(2)
+    arg_name, arg_val = arg.split('=')
+    try:
+        setattr(this_module, arg_name, float(arg_val))
+    except:
+        setattr(this_module, arg_name, arg_val)
+
+params = [
+    ('env_openness',.5),
+    ('tolerance',.5),
+    ('sweep',0),
+    ('seed',0),
+    ('logLevel','INFO')]
+for (param,default) in params:
+    if not hasattr(this_module, param):
+        setattr(this_module, param, default)
+
+if logLevel == 'NONE':
+    logging.getLogger().setLevel(logging.CRITICAL + 1)
 else:
+    logging.getLogger().setLevel(logLevel)
+
+if seed in (0,None):
     seed = random.randrange(10000)
 
-if len(sys.argv) > 3:
-    do_sweep = sys.argv[3].startswith('sweep=')
-    if do_sweep:
-        try:
-            num_runs = int(sys.argv[3][sys.argv[3].index('=')+1:])
-        except:
-            print_usage()
-            print("('sweep=#' parameter must be numeric.)")
-            sys.exit(5)
-else:
-    do_sweep = False
-
-params = [('TOLERANCE',.5),('ENV_OPENNESS',.5)]
-this_module = sys.modules[__name__]
-for i,(param,default) in enumerate(params):
-    if len(sys.argv) > i+1:
-        try:
-            setattr(this_module, param, float(sys.argv[i+1]))
-            if not 0 <= float(sys.argv[i+1]) <= 1:
-                raise exception
-        except:
-            print("{} '{}' must be float in (0,1).".format(param.title(),
-                                                            sys.argv[i+1]))
-            sys.exit(10+i)
-    else:
-        setattr(this_module, param, default)
 
 
 # Other configuration parameters.
 N = 50
 MIN_FRIENDS_PER_NEIGHBOR = 3
 NUM_IDEOLOGIES = 3
-NUM_ITER = 100
+NUM_ITER = 200
 
 
-if do_sweep:
+if sweep:
     pass
 else:
     print('=== Using seed {}.'.format(seed))
-    print("=== TOLERANCE={}, ENV_OPENNESS={}, N={}, MIN_FRIENDS={}.".format(
-        TOLERANCE, ENV_OPENNESS, N, MIN_FRIENDS_PER_NEIGHBOR))
+    print("=== tolerance={}, env_openness={}, N={}, MIN_FRIENDS={}.".format(
+        tolerance, env_openness, N, MIN_FRIENDS_PER_NEIGHBOR))
     print("=== NUM_ITER={}, NUM_IDEOLOGIES={}.".format(NUM_ITER, 
                                                             NUM_IDEOLOGIES))
     random.seed(seed)
 
     associates_graph = generate_associates_graph(N, MIN_FRIENDS_PER_NEIGHBOR,
         NUM_IDEOLOGIES)
-    graph = generate_friends_graph(associates_graph, ENV_OPENNESS, TOLERANCE,
+    graph = generate_friends_graph(associates_graph, env_openness, tolerance,
         MIN_FRIENDS_PER_NEIGHBOR)
     results = run_bvm(graph, NUM_ITER, True)
     with open('/tmp/results.csv','w') as f:
