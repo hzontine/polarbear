cabc63e9f515b1924473de11a594229fda37778e
Stephen <stephen@umw.edu>
2017-07-26 11:33:24 -0400

    Further improve command-line args.

--- wide/main_wide.py
+++ wide/main_wide.py
@@ -12,9 +12,10 @@ from wide_sim import *
 
 def print_usage():
     print('Usage: main_wide.py [env_openness=#] [tolerance=#] [sweep=#] ' +
-        '[seed=#] [logLevel=level|NONE].')
+        '[num_iter=#] [plot_graphs=True|False] ' +
+        '[seed=#] [log_level=level|NONE].')
 
-if len(sys.argv) > 5:
+if len(sys.argv) > 8:
     print_usage()
     sys.exit(1)
 
@@ -27,24 +28,29 @@ for arg in sys.argv[1:]:
         sys.exit(2)
     arg_name, arg_val = arg.split('=')
     try:
-        setattr(this_module, arg_name, float(arg_val))
+        setattr(this_module, arg_name, int(arg_val))
     except:
-        setattr(this_module, arg_name, arg_val)
+        try:
+            setattr(this_module, arg_name, float(arg_val))
+        except:
+            setattr(this_module, arg_name, arg_val)
 
 params = [
     ('env_openness',.5),
     ('tolerance',.5),
     ('sweep',0),
     ('seed',0),
-    ('logLevel','INFO')]
+    ('num_iter',200),
+    ('plot_graphs',True),
+    ('log_level','INFO')]
 for (param,default) in params:
     if not hasattr(this_module, param):
         setattr(this_module, param, default)
 
-if logLevel == 'NONE':
+if log_level == 'NONE':
     logging.getLogger().setLevel(logging.CRITICAL + 1)
 else:
-    logging.getLogger().setLevel(logLevel)
+    logging.getLogger().setLevel(log_level)
 
 if seed in (0,None):
     seed = random.randrange(10000)
@@ -55,7 +61,6 @@ if seed in (0,None):
 N = 50
 MIN_FRIENDS_PER_NEIGHBOR = 3
 NUM_IDEOLOGIES = 3
-NUM_ITER = 200
 
 
 if sweep:
@@ -64,7 +69,7 @@ else:
     print('=== Using seed {}.'.format(seed))
     print("=== tolerance={}, env_openness={}, N={}, MIN_FRIENDS={}.".format(
         tolerance, env_openness, N, MIN_FRIENDS_PER_NEIGHBOR))
-    print("=== NUM_ITER={}, NUM_IDEOLOGIES={}.".format(NUM_ITER, 
+    print("=== num_iter={}, NUM_IDEOLOGIES={}.".format(num_iter, 
                                                             NUM_IDEOLOGIES))
     random.seed(seed)
 
@@ -72,7 +77,7 @@ else:
         NUM_IDEOLOGIES)
     graph = generate_friends_graph(associates_graph, env_openness, tolerance,
         MIN_FRIENDS_PER_NEIGHBOR)
-    results = run_bvm(graph, NUM_ITER, True)
+    results = run_bvm(graph, num_iter, True if plot_graphs=='True' else False)
     with open('/tmp/results.csv','w') as f:
         results.to_csv(f)
     print('results at /tmp/results.csv.')
