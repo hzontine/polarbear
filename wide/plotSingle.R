dd13c90d1f48965ac228aa62f82af69d5277a750
Stephen <stephen@umw.edu>
2017-07-26 11:16:34 -0400

    Adjust axis limits.

--- wide/plotSingle.R
+++ wide/plotSingle.R
@@ -11,7 +11,9 @@ results <- read.csv(filename,header=TRUE)
 
 g <- ggplot(results, aes(x=iteration, y=assortativity)) + 
     geom_point() + 
-    geom_smooth(method="loess")
+    geom_smooth(method="loess") +
+    ylim(-.7,.7) +
+    geom_hline(yintercept=0, color="darkgrey")
 image_name <- str_replace(filename, '.csv', '.png')
 ggsave(image_name, g)
 system(paste("eog",image_name))
