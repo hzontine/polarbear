ba168942ab43e440f20bef78667feeaecd6ce7eb
Stephen <stephen@umw.edu>
2017-07-27 10:56:25 -0400

    Add plot_suite param to switch to file/stdout.
    Add scaffolding for sweeps.

--- wide/main_wide.py
+++ wide/main_wide.py
@@ -13,9 +13,34 @@ from suite import *
 
 
 def print_usage():
-    print('Usage: main_wide.py [env_openness=#] [tolerance=#] [suite=#] ' +
-        '[num_iter=#] [plot_graphs=True|False] ' +
-        '[seed=#] [log_level=level|NONE].')
+    print('Usage: main_wide.py [env_openness=#|range]\n' +
+        '                    [tolerance=#|range]\n' +
+        '                    [suite=#]\n' +
+        '                    [num_iter=#]\n' +
+        '                    [plot_graphs=True|False]\n' +
+        '                    [plot_suite=True|False]\n' +
+        '                    [seed=#]\n' +
+        '                    [log_level=level|NONE].\n' +
+        '(where "range" is #-#-#, for start-stop-step.)')
+
+
+# https://stackoverflow.com/questions/17602878/ ##############################
+import sys
+import contextlib
+
+@contextlib.contextmanager
+def smart_open(filename=None):
+    if filename and filename != 'stdout':
+        fh = open(filename, 'w')
+    else:
+        fh = sys.stdout
+    try:
+        yield fh
+    finally:
+        if fh is not sys.stdout:
+            fh.close()
+##############################################################################
+
 
 if len(sys.argv) > 8:
     print_usage()
@@ -37,6 +62,7 @@ for arg in sys.argv[1:]:
         except:
             setattr(this_module, arg_name, arg_val)
 
+sweepable_params = [ 'env_openness', 'tolerance' ]
 params = [
     ('env_openness',.5),
     ('tolerance',.5),
@@ -44,6 +70,7 @@ params = [
     ('seed',0),
     ('num_iter',200),
     ('plot_graphs',True),
+    ('plot_suite',True),
     ('log_level','INFO')]
 param_dict = {}
 for (param,default) in params:
@@ -70,17 +97,28 @@ param_dict['MIN_FRIENDS_PER_NEIGHBOR'] = MIN_FRIENDS_PER_NEIGHBOR
 param_dict['NUM_IDEOLOGIES'] = NUM_IDEOLOGIES
 
 
-_,filename = tempfile.mkstemp(suffix=".csv",dir="/tmp")
-if suite:
+if any([ type(param_dict[x]) == str for x in sweepable_params ]):
+    # Parameter sweep (over different parameter values).
+    print("sweep.")
+    pass
+elif suite:
+    # Suite of runs (with all the same parameters).
+    if plot_suite in [True,'True']:
+        _,filename = tempfile.mkstemp(suffix=".csv",dir="/tmp")
+    else:
+        filename = 'stdout'
+        # Turn off logging to not interfere with stdout.
+        logging.getLogger().setLevel(logging.CRITICAL + 1)
     suite_results = Suite(range(seed,seed+suite)).run(param_dict)
-    with open(filename,'w') as f:
-        print('seed,iteration,assortativity',file=f)
+    with smart_open(filename) as f:
+        print('seed,iteration,assortativity', file=f)
         for seed,results in suite_results.items():
             for i,r in enumerate(results):
                 print('{},{},{:.4f}'.format(seed,i,r),file=f)
-    print('results at {}.'.format(filename))
-    os.system('./plotSuite.R --args {}'.format(filename))
+    if plot_suite in [True,'True']:
+        os.system('./plotSuite.R --args {}'.format(filename))
 else:
+    # Single run.
     print('=== Using seed {}.'.format(seed))
     print("=== tolerance={}, env_openness={}, N={}, MIN_FRIENDS={}.".format(
         tolerance, env_openness, N, MIN_FRIENDS_PER_NEIGHBOR))
@@ -93,6 +131,7 @@ else:
     graph = generate_friends_graph(associates_graph, env_openness, tolerance,
         MIN_FRIENDS_PER_NEIGHBOR)
     results = run_bvm(graph, num_iter, True if plot_graphs=='True' else False)
+    _,filename = tempfile.mkstemp(suffix=".csv",dir="/tmp")
     with open(filename,'w') as f:
         print('iteration,assortativity',file=f)
         for i,r in enumerate(results):
